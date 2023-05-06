a83642ea6ed5f4ed8379c93161494f61f5a3e812
miku1001 <pauloted.22@gmail.com>
2023-05-06 19:58:16 +0800

    Create a Menu

--- calculator.py
+++ calculator.py
@@ -1,4 +1,12 @@
+# import pyfiglet to change font color and style
+
+import pyfiglet
 # Display Operation Menu
+print(pyfiglet.figlet_format("Simple Mini Calculator".center(50), font = "digital"))
+print("To choose your desire operation: ")
+print("\033[31mPress 1 \033[0m: Addition", " " * 35, "\033[32mPress 2 \033[0m: Subtraction \n"
+      "\033[33mPress 3 \033[0m: Multiplication ", " " * 28, "\033[34mPress 4 \033[0m: Division")
+print ("=" * 90)
 # Ask the user for operation
 # Press 1 for addition
     # Enter number
