e0acf257ab669a41ca04773e287f13838c53a8fe
miku1001 <pauloted.22@gmail.com>
2023-05-06 19:20:41 +0800

    Pseudocodes

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# Display Operation Menu
+# Ask the user for operation
+# Press 1 for addition
+    # Enter number
+    # Enter second number
+    # print result
+# Press 2 for subtraction
+    # Enter number
+    # Enter second number
+    # print result
+# Press 3 for multiplication
+    # Enter number
+    # Enter second number
+    # print result
+# Press 4 for divison
+    # Enter number
+    # Enter second number
+    # print result
+    # If user input 0 in second number
+# Ask the user to continue or stop the program
+# If n
+    # Stop the program
+# if y
+    # Back to the first part
+
+
+
