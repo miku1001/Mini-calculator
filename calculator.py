e4e6d72593cdde5370a17f7285fbadcfcda97df7
miku1001 <pauloted.22@gmail.com>
2023-05-06 20:35:15 +0800

    Mode choices

--- calculator.py
+++ calculator.py
@@ -1,13 +1,24 @@
 # import pyfiglet to change font color and style
 
 import pyfiglet
+
 # Display Operation Menu
 print(pyfiglet.figlet_format("Simple Mini Calculator".center(50), font = "digital"))
 print("To choose your desire operation: ")
 print("\033[31mPress 1 \033[0m: Addition", " " * 35, "\033[32mPress 2 \033[0m: Subtraction \n"
       "\033[33mPress 3 \033[0m: Multiplication ", " " * 28, "\033[34mPress 4 \033[0m: Division")
 print ("=" * 90)
+
+def calculator():
+    while True:
+        try:
 # Ask the user for operation
+            mode = int(input("Enter Operation: "))
+
+        # Present
+        except ValueError:
+            print("ERROR! The number that you input is invalid")
+            print()
 # Press 1 for addition
     # Enter number
     # Enter second number
@@ -25,6 +36,8 @@ print ("=" * 90)
     # Enter second number
     # print result
     # If user input 0 in second number
+# start
+calculator()
 # Ask the user to continue or stop the program
 # If n
     # Stop the program
